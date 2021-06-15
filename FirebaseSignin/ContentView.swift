32f99609ffae3b2d59f649ece0080cc42caa6560
RuiOkazaki <okazaki200226@gmail.com>
2021-06-15 20:35:45 +0900

    first commit

---
+++ FirebaseSignin/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FirebaseSignin
+//
+//  Created by 岡崎流依 on 2021/06/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FirebaseSignin/FirebaseSigninApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseSigninApp.swift
+//  FirebaseSignin
+//
+//  Created by 岡崎流依 on 2021/06/15.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseSigninApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
